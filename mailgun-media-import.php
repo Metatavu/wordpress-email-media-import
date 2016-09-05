9059b06cecf0e013e1b6620bc8a8bccaaad0e477
Antti Leppä <antti.leppa@foyt.fi>
2016-09-05 18:43:49 +0300

    Changed rewrite to redirect because rewrite did not work

--- mailgun-media-import.php
+++ mailgun-media-import.php
@@ -10,14 +10,13 @@
 defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 
 require_once("settings.php");
+require_once(ABSPATH . 'wp-includes/pluggable.php');
 
-add_action( 'init', 'mailgunMediaImportRegisterRewrite' );
+$path = $_SERVER['REQUEST_URI'];
 
-function mailgunMediaImportRegisterRewrite() {
-  global $wp_rewrite;
-  $plugin_url = plugins_url('import.php', __FILE__);
-  $wp_rewrite->add_external_rule('mailgun-media-import.php$', $plugin_url);
-  $wp_rewrite->flush_rules();
+if ($path == "/mailgun-media-import") {
+  wp_redirect(plugins_url('import.php', __FILE__));
+  exit;
 }
 
 ?>
