398acbab1fb446b7636b3e4814b41d12e5e47101
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-16 13:29:19 +0300

    Bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,25 +1,3 @@
 <?php
-/**
- * PHPUnit bootstrap file
- *
- * @package Email_Media_Import
- */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
-
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/email-media-import.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+?>
--- tests/test-upload.php
+++ tests/test-upload.php
@@ -1,25 +1,61 @@
 <?php
 /**
- * Class SampleTest
+ * Class UploadTest
  *
  * @package Email_Media_Import
  */
 
 /**
- * Sample test case.
+ * Upload test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class UploadTest extends WP_UnitTestCase {
 	
 	/**
-	 * A single example test.
+	 * Tests image uploadn
 	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+	function testUpload() {
+	  $pageId = $this->factory->post->create(array(
+        'post-type' => 'page',
+	    'post_title' => 'import',
+	    'post_content' => '[email_media_import]',
+	    'post_status' => 'published'
+	  ));
+		
+	  $this->assertNotNull($pageId);
+	  
+	  var_dump($this->factory->post->get_object_by_id($pageId));
+	  
+		// $_POST = $this->createPostRequestData("ruumis", "123", "123", "123", "image/png", "http://fake.example.com/file.png", 12345);
+		// var_dump($_POST);
 	}
 	
-
-	function createPostData($bodyPlain, $token, $signature, $imageName, $imageType, $imageUrl, $imageSize) {
+	function getRequest($url) {
+	  return  file_get_contents($url, false, null);
+	}
+	
+	
+	/**
+	function mockWebHook() {
+	  $url = "http://localhost";
+	  
+		$url = 'http://server.com/path';
+		$data = array('key1' => 'value1', 'key2' => 'value2');
+		
+		// use key 'http' even if you send the request to https://...
+		$options = array(
+				'http' => array(
+						'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+						'method'  => 'POST',
+						'content' => http_build_query($data)
+				)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		if ($result === FALSE) { Handle error  }
+		
+		var_dump($result);	}
+		**/
+	function createPostRequestData($bodyPlain, $token, $signature, $imageName, $imageType, $imageUrl, $imageSize) {
 	  $fromName = "Someone Special";
 	  $fromMail = "someone-special@example.com";
 	  $from = "\"$fromName\" <$fromMail>";
