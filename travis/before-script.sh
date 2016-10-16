f61b09c80d61bb77bcfd730702dc92a655e90fee
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-16 10:51:51 +0300

    Nginx

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+BASE=`pwd`
+
 # Setup MySQL
 mysql -e 'create database www;'
 
@@ -7,10 +9,10 @@ mysql -e 'create database www;'
 curl -sS -o /tmp/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x /tmp/wp
 
 # Setup php-fmt
-php-fpm --fpm-config `pwd`/travis/php-fmt.conf
+php-fpm --fpm-config $BASE/travis/php-fmt.conf
 
 # Setup nginx
-nginx -c `pwd`/travis/nginx.conf
+nginx -c $BASE/travis/nginx.conf
 
 # Setup wp
 mkdir /tmp/www
@@ -18,10 +20,11 @@ cd /tmp/www
 /tmp/wp core download
 /tmp/wp core config --dbname=www --dbuser=root
 /tmp/wp core install --url=http://localhost --title=Test --admin_user=admin --admin_password=password --admin_email=admin@example.com
+/tmp/wp plugin install file://$BASE
 
 # Just test
 echo "Curling /"
 curl http://localhost:8080
 
-echo "Curling /wp-admin"
-curl http://localhost:8080/wp-admin
+echo "Curling /index.php"
+curl http://localhost:8080/index.php
