8334ae6757a2333fc324204b25cabebf6c99d9ca
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-22 10:04:34 +0300

    Grunt

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -13,8 +13,6 @@ mkdir -p /tmp/bin
 curl -sS -o /tmp/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x /tmp/bin/wp
 export PATH="$PATH:/tmp/bin"
 
-wp
-
 # Setup grunt
 npm install
 
