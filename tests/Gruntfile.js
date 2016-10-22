67e9f4651fb7f2ac9a6e226231b1820c5e32eaa4
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-22 17:16:05 +0300

    Coverage

--- tests/Gruntfile.js
+++ tests/Gruntfile.js
@@ -165,7 +165,7 @@ module.exports = function(grunt) {
     },
     'bgShell': {
       "start-wordpress-server-background": {
-        cmd: "php -e -S " + config.wordpress.site.url + " & echo $! > /tmp/wordpress-server.pid",
+        cmd: "php -d xdebug.remote_port=2345 -e -S " + config.wordpress.site.url + " & echo $! > /tmp/wordpress-server.pid",
         bg: true,
         execOpts: {
           cwd: config.wordpress.path
