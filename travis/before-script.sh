c3e4c0bec74ccb1a203fb2c82b855c1df1f5cf82
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-22 08:46:31 +0300

    Added support for running grunt scripts

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -2,7 +2,20 @@
 
 BASE=`pwd`
 
+# Copy config
+cp $BASE/travis/grunt-config.js $BASE/grunt-config.js
+
 # Install wp-cli
 mkdir -p /tmp/bin
 curl -sS -o /tmp/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x /tmp/bin/wp
 export PATH="$PATH:/tmp/bin"
+
+# Setup grunt
+npm install
+
+# install wordpress 
+grunt install-wordpress
+
+# Start server
+grunt start-server
+grunt mock-data
