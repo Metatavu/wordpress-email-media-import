f03657527b846179f434225593725fef3061ead5
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-22 08:00:56 +0300

    Removed old travis setup

--- travis/after-script.sh
+++ travis/after-script.sh
@@ -1,7 +1,1 @@
 #/bin/bash
-
-echo "Nginx error log:" 
-cat /tmp/error.log
-
-echo "Nginx access log:" 
-cat /tmp/access.log
--- travis/before-script.sh
+++ travis/before-script.sh
@@ -2,29 +2,7 @@
 
 BASE=`pwd`
 
-# Update dependencies
-composer install
-
-# Setup MySQL
-mysql -e 'create database www;'
-
 # Install wp-cli
-curl -sS -o /tmp/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x /tmp/wp
-
-# Setup php-fmt
-php-fpm --fpm-config $BASE/travis/php-fmt.conf
-
-# Setup wp
-mkdir /tmp/www
-cd /tmp/www
-/tmp/wp core download
-/tmp/wp core config --dbname=www --dbuser=root
-/tmp/wp core install --url=http://localhost:8080 --title=Test --admin_user=admin --admin_password=password --admin_email=admin@example.com
-ln -s $BASE /tmp/www/wp-content/plugins/email-media-import
-/tmp/wp plugin activate email-media-import
-/tmp/wp plugin install rest-api --activate
-/tmp/wp option update permalink_structure /archives/%post_id%
-/tmp/wp post create $BASE/tests/import-contents.txt --post_type=page --post_title='import'
-
-# Start nginx
-nginx -c $BASE/travis/nginx.conf
+mkdir -p /tmp/bin
+curl -sS -o /tmp/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod a+x /tmp/bin/wp
+export PATH="$PATH:/tmp/bin"
